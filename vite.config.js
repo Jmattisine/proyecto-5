2cf2d3add6bb0bf25c45addbdb99d1158a3b21d5
Jonathan Mattisine <mattisine.jonathan@gmail.com>
2025-08-13 17:26:51 -0400

    Configuración corregida para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/proyecto-5/',
   plugins: [react()],
-  base: '/proyecto-5/', 
 });
+
